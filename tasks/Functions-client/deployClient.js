8cd94d3386e463541fe2ccb1eac968b165b26629
Frank Kong <frankkongnj@gmail.com>
2023-03-02 17:11:30 +0800

    update tasks for parametric insurance

--- tasks/Functions-client/deployClient.js
+++ tasks/Functions-client/deployClient.js
@@ -1,25 +1,26 @@
 const { types } = require("hardhat/config")
 const { VERIFICATION_BLOCK_CONFIRMATIONS, networkConfig } = require("../../network-config")
 
-task("functions-deploy-client", "Deploys the FunctionsConsumer contract")
+task("functions-deploy-client", "Deploys the ParametricInsurance contract")
   .addOptionalParam("verify", "Set to true to verify client contract", false, types.boolean)
   .setAction(async (taskArgs) => {
     if (network.name === "hardhat") {
       throw Error(
-        'This command cannot be used on a local hardhat chain.  Specify a valid network or simulate an FunctionsConsumer request locally with "npx hardhat functions-simulate".'
+        'This command cannot be used on a local hardhat chain.  Specify a valid network or simulate an ParametricInsurance request locally with "npx hardhat functions-simulate".'
       )
     }
 
-    console.log(`Deploying FunctionsConsumer contract to ${network.name}`)
+    console.log(`Deploying ParametricInsurance contract to ${network.name}`)
 
     const oracleAddress = networkConfig[network.name]["functionsOracleProxy"]
+    const requestConfig = require("../../Functions-request-config.js")
 
     console.log("\n__Compiling Contracts__")
     await run("compile")
 
-    const clientContractFactory = await ethers.getContractFactory("FunctionsConsumer")
+    const clientContractFactory = await ethers.getContractFactory("ParametricInsurance")
 
-    const clientContract = await clientContractFactory.deploy(oracleAddress)
+    const clientContract = await clientContractFactory.deploy(oracleAddress, requestConfig.secrets.clientAddress)
 
     console.log(
       `\nWaiting ${VERIFICATION_BLOCK_CONFIRMATIONS} blocks for transaction ${clientContract.deployTransaction.hash} to be confirmed...`
@@ -34,7 +35,7 @@ task("functions-deploy-client", "Deploys the FunctionsConsumer contract")
         await clientContract.deployTransaction.wait(Math.max(6 - VERIFICATION_BLOCK_CONFIRMATIONS, 0))
         await run("verify:verify", {
           address: clientContract.address,
-          constructorArguments: [oracleAddress],
+          constructorArguments: [oracleAddress, requestConfig.secrets.clientAddress],
         })
         console.log("Contract verified")
       } catch (error) {
@@ -49,5 +50,5 @@ task("functions-deploy-client", "Deploys the FunctionsConsumer contract")
       console.log("\nPOLYGONSCAN_API_KEY or ETHERSCAN_API_KEY missing. Skipping contract verification...")
     }
 
-    console.log(`\nFunctionsConsumer contract deployed to ${clientContract.address} on ${network.name}`)
+    console.log(`\ParametricInsurance contract deployed to ${clientContract.address} on ${network.name}`)
   })
